84c78c7aa2573114e79b4462e3ec076cd016c88f
geofrano <geofrano@hotmail.com>
2018-03-24 18:59:46 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,52 +1,17 @@
-console.log('El bot esta listo');
-const botSettings = require('./botsettings.json');
 const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const botSettings = require('./botsettings.json');
 const prefix = botSettings.prefix;
 
-//const bot = new Discord.Client();
-const bot = new Discord.Client({disableEveryone: true});
-
-bot.on("ready", async () => {
-	console.log(`El bot esta listo! ${bot.user.username}`);
-	
-	try {
-		let link = await bot.generateInvite(["ADMINISTRATOR"]);
-		console.log(link);
-	} catch(e) {
-		console.log(e.stack);
-	}
-});	
-
-	
-bot.on('message', async message => {
-	
-	if (message.author.bot) return;
-	if (message.channel.type == "dm") return;
-	
-	let messageArray = message.content.split("	");
-	let command = messageArray[0];
-	let args = messageArray.slice(1);
-	
-	if (!command.startsWith(prefix)) return;
-	
-	if (command === `${prefix}userinfo`) {
-		let embed = new Discord.RichEmbed()
-			.setAuthor(message.author.username)
-			.setDescription("This is the user's info!")
-			.setColor("#9B59B6")
-			.addField("Full Username", `${message.author.username}#${message.author.discriminator}`)
-			.addField("ID", message.author.id)
-			.addField("Created At", message.author.createdAt);
-			
-		message.channel.sendEmbed(embed);
-		return;	
-	}
-	
-	
+client.on('ready', () => {
+    console.log('I am ready!');
 });
 
-
-bot.on('message', async message => {
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
 	
 	if (message.content == 'hola') {
 		message.channel.sendMessage('Hola ! :grin: ');
@@ -58,8 +23,9 @@ bot.on('message', async message => {
 	if (message.content == 'info almabot') {
 		message.channel.sendMessage('Aun estoy en pruebas');
 	}
-	
-	
 });
-//bot.login('NDE2NDIyMjcyNDY3OTkyNTg3.DXEPBw.1uNZFrViwOosX18W_MdB7Z2LDSs'); 
+
+
+
+// THIS  MUST  BE  THIS  WAY
 bot.login(botSettings.token);
