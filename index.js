e8dbabd253c96fcf78fc95a7e31b46c5ef4929f0
geofrano <geofrano@hotmail.com>
2018-03-24 19:54:00 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,65 @@
 console.log('El bot esta listo');
+const botSettings = require('./botsettings.json');
 const Discord = require('discord.js');
-const bot = new Discord.Client();
+const prefix = botSettings.prefix;
 
-bot.on('message', (message) => {
+//const bot = new Discord.Client();
+const bot = new Discord.Client({disableEveryone: true});
+
+bot.on("ready", async () => {
+	console.log(`El bot esta listo! ${bot.user.username}`);
+	
+	try {
+		let link = await bot.generateInvite(["ADMINISTRATOR"]);
+		console.log(link);
+	} catch(e) {
+		console.log(e.stack);
+	}
+});	
+
+	
+bot.on('message', async message => {
+	
+	if (message.author.bot) return;
+	if (message.channel.type == "dm") return;
+	
+	let messageArray = message.content.split("	");
+	let command = messageArray[0];
+	let args = messageArray.slice(1);
+	
+	if (!command.startsWith(prefix)) return;
+	
+	if (command === `${prefix}userinfo`) {
+		let embed = new Discord.RichEmbed()
+			.setAuthor(message.author.username)
+			.setDescription("This is the user's info!")
+			.setColor("#9B59B6")
+			.addField("Full Username", `${message.author.username}#${message.author.discriminator}`)
+			.addField("ID", message.author.id)
+			.addField("Created At", message.author.createdAt);
+			
+		message.channel.sendEmbed(embed);
+		return;	
+	}
+	
+	
+});
+
+
+bot.on('message', async message => {
+	
 	if (message.content == 'hola') {
-		message.channel.sendMessage('Hola ! ');
+		message.channel.sendMessage('Hola ! :grin: ');
+		
 	}
 	if (message.content == 'quien es el mas pto?') {
-		message.channel.sendMessage('Bilbo');
+		message.channel.sendMessage('Bilbo :speaking_head: ');
 	}
-	if (message.content == 'info geobot') {
-		message.channel.sendMessage('Soy solo una prueba pto nano :v');
+	if (message.content == 'info almabot') {
+		message.channel.sendMessage('Aun estoy en pruebas');
 	}
-	if (message.content == 'que agradable sujeto') {
-		message.channel.sendMessage(':grin:');
-	}
+	
+	
 });
-bot.login('NDE1MzM2NjIxMzUzODYxMTIy.DW0lrg.CAr8vkcPoKdc6njfxkLCjOmhYcY'); 
+//bot.login('NDE2NDIyMjcyNDY3OTkyNTg3.DXEPBw.1uNZFrViwOosX18W_MdB7Z2LDSs'); 
+bot.login(botSettings.token);
